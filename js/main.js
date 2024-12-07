370edad24bf76b5ddaec018c333ab3f9c24692c0
unknown <renanhvieira@hotmail.com>
2024-12-07 16:05:46 -0300

    tarefa

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+    autoplay:true
+    });
+
+    $('.menu-hamburger').click(function() {
+        $('nav').slideToggle();
+    })
+
+
+
+
+
+})
